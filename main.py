52a00b607e20e218d5a15fb28699aa24ebc1cb9c
Kellrond <66251615+Kellrond@users.noreply.github.com>
2020-10-11 03:58:15 -0700

    Started adding in pairs but now hands have duplicate cards. Test to find where the lists are getting mixed. Likely a type

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from enum import Enum
 from enum import IntEnum
 from random import *
+from collections import Counter
 
 fullDeck = []
 playDeck = []
+thePlay = []
 p1score = 0
 p2score = 0
 dealer = bool
 canGo = bool
 playCount = 0
 roundCounter = 1
-WIN = 12
+WIN = 5
+
 
 
 # # Card enum for cards
@@ -164,30 +167,94 @@ def playableCards(cardsinhand, count):
     return playable
 
 
-def p1play(playCount):
-    playable = playableCards(p1playhand, playCount)
+def suggestedCard(cardsinhand, count, thePlay):
+    suggestion = None
 
-    randCard = playable[randint(0, len(playable) - 1)]
-    playCard = p1playhand.pop(randCard)
+    if len(thePlay) == 0:
+        suggestion = None
+    else:
+        for index, c in enumerate(cardsinhand):
+            samplePlay = thePlay.copy()
+
+            if playPairs(samplePlay.append(c)) > 0:
+                suggestion = cardsinhand.pop(index)
+            elif c.value + count == 15:
+                suggestion = cardsinhand.pop(index)
+            elif c.value + count == 31:
+                suggestion = cardsinhand.pop(index)
+    return suggestion
+
+
+def p1play(playCount, thePlay):
+    playable = playableCards(p1playhand, playCount)
+    suggestion = suggestedCard(p1playhand, playCount, thePlay)
+    if suggestion is not None:
+        playCard = suggestion
+    else:
+        randCard = playable[randint(0, len(playable) - 1)]
+        playCard = p1playhand.pop(randCard)
+
     playCount += playCard.value
     thePlay.append(playCard)
     print("P1:",playCard.textPlay(),"Count:",playCount)
 
-
     return playCount
 
 
-
-def p2play(playCount):
+def p2play(playCount, thePlay):
     playable = playableCards(p2playhand, playCount)
+    suggestion = suggestedCard(p2playhand, playCount, thePlay)
+    if suggestion is not None:
+        playCard = suggestion
+    else:
+        randCard = playable[randint(0, len(playable) - 1)]
+        playCard = p2playhand.pop(randCard)
 
-    randCard = playable[randint(0, len(playable) - 1)]
-    playCard = p2playhand.pop(randCard)
     playCount += playCard.value
     thePlay.append(playCard)
     print("P2:", playCard.textPlay(), "Count:", playCount)
 
     return playCount
+
+
+def checkPlayForPoints(playCount, thePlay):
+    count = playCount
+    play = thePlay
+    score = 0
+    if playCount == 15:
+        score += 2
+        print("15 two")
+    elif playCount == 31:
+        score += 1
+        print("31 for two")
+    score += playPairs(thePlay)
+
+    return score
+
+
+def playPairs(play):
+    pairsList = thePlay.copy()
+    pairsList.reverse()
+    score = 0
+
+    # TODO refine this code
+    if Counter(pairsList[0:1]).values() == 2:
+        if Counter(pairsList[0:2]).values() == 3:
+            if Counter(pairsList[0:3]).values() == 4:
+                print("Four of a kind @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+                score = 12
+            else:
+                print("Three of a kind @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+                score = 6
+        else:
+            print("Two of a kind @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            score = 2
+    return score
+
+
+
+
+
 
 
 # # Game starts
@@ -205,101 +272,111 @@ print()
 # # Prep the play phase
 print("───Play Phase")
 
-roundPhaseOver = False
-while not roundPhaseOver:
-    print(roundCounter,"────¼─────")
-    createDeck()
-    playDeck = list(fullDeck)
+for i in range(0, 1):
+    roundPhaseOver = False
+    while not roundPhaseOver:
+        print(roundCounter,"────¼─────")
+        createDeck()
+        playDeck = list(fullDeck)
 
-    player1cards = []
-    player2cards = []
-    crib = []
-    thePlay = []
+        player1cards = []
+        player2cards = []
+        crib = []
+        thePlay = []
 
-    deal()
+        deal()
 
-    p2discard()
-    p1discard()
+        p2discard()
+        p1discard()
 
-    p1playhand = player1cards.copy()
-    p2playhand = player2cards.copy()
+        p1playhand = player1cards.copy()
+        p2playhand = player2cards.copy()
 
-    if dealer:
-        p1turn = False
-    else:
-        p1turn = True
-
-    if p1turn:
-        print("Player 1 starts")
-    else:
-        print("Player 2 starts")
-
-    outOfCards = False
-    while not outOfCards:
-        if len(p1playhand) == 0 and len(p2playhand) == 0:
-            outOfCards = True
+        if dealer:
+            p1turn = False
         else:
-            playCount = 0
-            p1go = False
-            p2go = False
+            p1turn = True
 
-            go = False
-            while not go:
-                if p1go is True and p2go is True:
-                    go = True
-                    print("Go!", playCount)
+        if p1turn:
+            print("Player 1 starts")
+        else:
+            print("Player 2 starts")
 
-                    if p1turn:
-                        p2score += 1
-                        print("Player 2 Scores - Total -", p2score)
-                    else:
-                        p1score += 1
-                        print("Player 1 Scores - Total -", p1score)
+        outOfCards = False
+        while not outOfCards:
+            if len(p1playhand) == 0 and len(p2playhand) == 0:
+                outOfCards = True
+            else:
+                playCount = 0
+                p1go = False
+                p2go = False
 
-                    for c in thePlay:
-                        print(c.textPlay(), end="\t")
+                go = False
+                while not go:
+                    if p1go is True and p2go is True:
+                        go = True
+                        print("Go!", playCount)
+                        if p1turn:
+                            p2score += 1
+                            print("Player 2 Scores - Total -", p2score)
+                        else:
+                            p1score += 1
+                            print("Player 1 Scores - Total -", p1score)
+                        for c in thePlay:
+                            print(c.textPlay(), end="\t")
+                        print()
+                        thePlay = []
 
-                    print()
-                    thePlay = []
+                    elif p1turn and canGo(p1playhand, playCount):
+                        playCount = p1play(playCount, thePlay)
+                        if len(thePlay) > 1:
+                            p1score += checkPlayForPoints(playCount, thePlay)
+                        p1turn = False
+                    elif p1turn:
+                        p1go = True
 
-                elif p1turn and canGo(p1playhand, playCount):
-                    playCount = p1play(playCount)
-                    p1turn = False
-                elif p1turn:
-                    p1go = True
-                    p1turn = False
-                elif not p1turn and canGo(p2playhand, playCount):
-                    playCount = p2play(playCount)
-                    p1turn = True
-                elif not p1turn:
-                    p1turn = True
-                    p2go = True
+                        p1turn = False
+                    elif not p1turn and canGo(p2playhand, playCount):
+                        playCount = p2play(playCount, thePlay)
+                        if len(thePlay) > 1:
+                            p2score += checkPlayForPoints(playCount, thePlay)
+                        p1turn = True
+                    elif not p1turn:
+                        p1turn = True
+                        p2go = True
 
-    print()
-    print("───Play phase over")
+        print()
+        print("───Play phase over")
 
-    print("Player 1")
-    for cards in player1cards: print(cards.textPlay())
-    print()
-    print("Player 2")
-    for cards in player2cards: print(cards.textPlay())
-    print()
-    print("Crib")
-    for cards in crib: print(cards.textPlay())
+        print("Player 1")
+        for cards in player1cards: print(cards.textPlay())
+        print()
+        print("Player 2")
+        for cards in player2cards: print(cards.textPlay())
+        print()
+        print("Crib")
+        for cards in crib: print(cards.textPlay())
 
-    print()
-    print("Player 1 Score -", p1score)
-    print("Player 2 score -", p2score)
+        print()
+        print("Player 1 Score -", p1score)
+        print("Player 2 score -", p2score)
+        print("─── Round",roundCounter)
 
-    if dealer:
-        dealer = False
-    else:
-        dealer = True
+        if dealer:
+            dealer = False
+        else:
+            dealer = True
 
-    if p1score >= WIN or p2score >= WIN:
-        roundPhaseOver = True
-    else:
-        roundPhaseOver = False
+        if p1score >= WIN or p2score >= WIN:
+            roundPhaseOver = True
+            with open("Roundtests.txt", "a") as fp:
+                fp.write(str(roundCounter) + ", ")
+            roundCounter = 0
+            p1score = 0
+            p2score = 0
+        else:
+            roundPhaseOver = False
+            roundCounter += 1
 
 
 
