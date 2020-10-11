683c3209416302fb22fc0a6401a36c777fa3c998
Kellrond <66251615+Kellrond@users.noreply.github.com>
2020-10-10 23:52:37 -0700

    Made the game loop and working on bugs in it

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from enum import IntEnum
 from random import *
 
 fullDeck = []
-partialDeck = []
+playDeck = []
 player1cards = []
 player2cards = []
+crib = []
+thePlay = []
 dealer = bool
+canGo = bool
+playCount = 0
 
 # # Card enum for cards
 class Card(IntEnum):
@@ -44,7 +48,7 @@ class PlayingCard:
         self.discard = discard
 
     def textPlay(self):
-        output = self.name.name + " " + self.suit.name
+        output = self.name.name.capitalize() + " " + self.suit.name.capitalize()
         return output
 
 
@@ -61,9 +65,9 @@ def createDeck():
 
 # # Draw single card from deck
 def cutDeck(deck):
-    input("Hit enter to cut the deck")
-    rand_card = randint(0, len(deck)-1)
-    return deck.pop(rand_card)
+    # input("Hit enter to cut the deck")
+    randCard = randint(0, len(deck)-1)
+    return deck.pop(randCard)
 
 
 def cutToStart():
@@ -72,59 +76,190 @@ def cutToStart():
     while not winnerFound:
 
         createDeck()
-        input("Cut and see who goes first")
-        cutDepth = randint(10, 40)
-        p1cut = fullDeck[cutDepth-1]
-        p2cut = cutDeck(fullDeck[cutDepth + 1:])
+        # input("Cut and see who goes first")
+        # cutDepth = randint(10, 40)
+        # p1cut = fullDeck[cutDepth-1]
+        # p2cut = cutDeck(fullDeck[cutDepth + 1:])
+
+        p1cut = fullDeck[randint(0, 26)]
+        p2cut = fullDeck[randint(27, 52)]
+
         if p1cut.numb < p2cut.numb:
             dealer = True
             winnerFound = True
             print("Player 1 cut - ", p1cut.textPlay())
-            print("Computer cut - ", p2cut.textPlay())
-            input("You win!! Your turn to deal")
+            print("Player 2 cut - ", p2cut.textPlay())
+            print("You win!! Your turn to deal")
+            
         elif p1cut.numb > p2cut.numb:
             dealer = False
             winnerFound = True
             print("Player 1 cut - ", p1cut.textPlay())
-            print("Computer cut - ", p2cut.textPlay())
-            input("You loose! Computer goes first")
+            print("Player 2 cut - ", p2cut.textPlay())
+            print("You loose! Computer goes first")
+            
         else:
             print("Player 1 cut - ", p1cut.textPlay())
-            print("Computer cut - ", p2cut.textPlay())
-            input("TIE! Draw again")
+            print("Player 2 cut - ", p2cut.textPlay())
+            print("TIE! Draw again")
             winnerFound = False
+            
     return dealer
 
 
 def deal():
-    partialDeck = list(fullDeck)
     if dealer:
         for i in range(0, 6):
-            player1cards.append(partialDeck.pop())
-            player2cards.append(partialDeck.pop())
+            player1cards.append(playDeck.pop())
+            player2cards.append(playDeck.pop())
+    else:
+        for i in range(0, 6):
+            player2cards.append(playDeck.pop())
+            player1cards.append(playDeck.pop())
 
 
-# dealer =  cutToStart()
+def p1discard():
+    # finished = False
+    # while not finished:
+    #
+    #     if len(player1cards) == 4:
+    #         finished = True
+    #     else:
+    #         for index, cards in enumerate(player1cards):
+    #             print(index, cards.textPlay())
+    #         discard = input("Select first cards to discard")
+    #         if int(discard) >= 0 or int(discard) <= 5:
+    #             crib.append(player1cards.pop(int(discard)))
+    #         else:
+    #             print("Please make a valid selection")
+    for i in range(0,2):
+        randCard = randint(0, len(player1cards) - 1)
+        crib.append(player1cards.pop(randCard))
 
+
+def p2discard():
+    for i in range(0,2):
+        randCard = randint(0, len(player2cards) - 1)
+        crib.append(player2cards.pop(randCard))
+
+
+def canGo(hand, count):
+
+    cango = False
+    for c in hand:
+        if c.value + count <= 31:
+            cango = True
+        else:
+            cango = False
+
+    return cango
+
+
+def playableCards(cards, count):
+
+    playable = []
+    for index, c in enumerate(cards):
+        if c.value + count <= 31:
+            playable.append(index)
+
+    return playable
+
+
+def p1play(playCount):
+    playable = playableCards(p1playhand, playCount)
+
+    randCard = playable[randint(0, len(playable) - 1)]
+    playCard = p1playhand.pop(randCard)
+    playCount += playCard.value
+    print("P1:",playCard.textPlay(),"Count:",playCount)
+
+
+    return playCard
+
+
+
+def p2play(playCount):
+    playable = playableCards(p2playhand, playCount)
+
+    randCard = playable[randint(0, len(playable) - 1)]
+    playCard = p2playhand.pop(randCard)
+    playCount += playCard.value
+    print("P2:", playCard.textPlay(), "Count:", playCount)
+
+    return playCard
+
+
+# # Game starts
+print("Game begins \n".upper())
 createDeck()
+playDeck = list(fullDeck)
 
-dealer = True
+
+dealer = cutToStart()
+
+print()
 
 deal()
 
+p2discard()
+p1discard()
 
 
 
 
+print("Player 1")
+for cards in player1cards: print(cards.textPlay())
+print()
+print("Player 2")
+for cards in player2cards: print(cards.textPlay())
+print()
+print("Crib")
+for cards in crib: print(cards.textPlay())
+
+print()
 
 
 
+# # Prep the play phase
+print("Play Phase")
+p1playhand = player1cards
+p2playhand = player2cards
 
+if dealer:
+    p1turn = False
+else:
+    p1turn = True
 
+playPhaseOver = False
+while not playPhaseOver:
 
+    if len(p1playhand) == 0 and len(p2playhand) == 0:
+        playPhaseOver = True
 
+    playCount = 0
 
+    p1go = False
+    p2go = False
+    go = False
+    while not go:
 
+        if p1go is True and p2go is True:
+            go = True
+            print("Go!")
+        elif p1turn and canGo(p1playhand, playCount):
+            thePlay.append(p1play(playCount))
+            p1turn = False
+        elif p1turn:
+            p1go = True
+            p1Turn = False
+        elif not p1turn and canGo(p2playhand, playCount):
+            thePlay.append(p2play(playCount))
+            p1Turn = True
+        elif not p1turn:
+            p2go = True
+            p1Turn = True
+
+print("Play phase over")
 
 
 
