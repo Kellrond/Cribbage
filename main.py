bfc8bc202bab6ab560550add24a8e043fee373e1
Kellrond <66251615+Kellrond@users.noreply.github.com>
2020-10-11 18:57:46 -0700

    Formatted text out to make it more human readable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from enum import IntEnum
 from random import *
+from statistics import mean
+import time
 from collections import Counter
 
 fullDeck = []
@@ -9,13 +11,11 @@ thePlay = []
 p1score = 0
 p2score = 0
 dealer = bool
-canGo = bool
 playCount = 0
 roundCounter = 1
 WIN = 121
 
 
-# # Card enum for cards
 class Card(IntEnum):
 
     ACE = 1
@@ -33,7 +33,6 @@ class Card(IntEnum):
     KING = 13
 
 
-# # Suit Enum for cards
 class Suit(Enum):
     SPADES = 'spades'
     CLUBS = 'clubs'
@@ -41,7 +40,6 @@ class Suit(Enum):
     DIAMONDS = 'diamonds'
 
 
-# # Class to hold card info
 class PlayingCard:
 
     def __init__(self, card_name, card_numb, card_suit, card_value,discard=False):
@@ -56,6 +54,9 @@ class PlayingCard:
         output = self.name.name.capitalize() + " " + self.suit.name.capitalize()
         return output
 
+# #
+# # Deck functions
+# #
 
 def createDeck():
 
@@ -69,7 +70,6 @@ def createDeck():
     shuffle(fullDeck)
 
 
-# # Draw single card from deck
 def cutDeck(deck):
 
     # input("Hit enter to cut the deck")
@@ -79,23 +79,25 @@ def cutDeck(deck):
 
 def cutToStart():
 
+    isDealer = False
     winnerFound = False
     while not winnerFound:
 
         createDeck()
 
+
         p1cut = fullDeck[randint(0, 26)]
         p2cut = fullDeck[randint(27, 52)]
 
         if p1cut.numb < p2cut.numb:
-            dealer = True
+            isDealer = True
             winnerFound = True
             print("Player 1 cut - ", p1cut.textPlay())
             print("Player 2 cut - ", p2cut.textPlay())
             print("You win!! Your turn to deal")
             
         elif p1cut.numb > p2cut.numb:
-            dealer = False
+            isDealer = False
             winnerFound = True
             print("Player 1 cut - ", p1cut.textPlay())
             print("Player 2 cut - ", p2cut.textPlay())
@@ -107,26 +109,25 @@ def cutToStart():
             print("TIE! Draw again")
             winnerFound = False
             
-    return dealer
+    return isDealer
 
 
 def deal():
 
-
-
-
-
-
+    shuffle(playDeck)
 
     if dealer:
         for i in range(0, 6):
-            player1cards.append(playDeck.pop())
             player2cards.append(playDeck.pop())
+            player1cards.append(playDeck.pop())
     else:
         for i in range(0, 6):
-            player2cards.append(playDeck.pop())
             player1cards.append(playDeck.pop())
+            player2cards.append(playDeck.pop())
 
+# #
+# # Player decision function
+# #
 
 def p1discard():
 
@@ -142,76 +143,121 @@ def p2discard():
         crib.append(player2cards.pop(randCard))
 
 
-def canGo(hand, count):
+def canGo(hand):
 
     cango = False
     for c in hand:
-        if c.value + count <= 31:
+        if c.value + playCount <= 31:
             cango = True
 
     return cango
 
 
-def playableCards(cardsinhand, count):
+def playableCards(cardsInHand):
 
     playable = []
-    for index, c in enumerate(cardsinhand):
-        if c.value + count <= 31:
+    for index, c in enumerate(cardsInHand):
+        if c.value + playCount <= 31:
             playable.append(index)
 
     return playable
 
 
-def suggestedCard(cardsinhand, count):
-    suggestion = None
+def suggestedCard(cardsInHand):
 
+    suggestion = None
     if len(thePlay) == 0:
         # # Todo add card suggestion
         suggestion = None
     else:
-        for index, c in enumerate(cardsinhand):
-            if testPairs(c):
-                suggestion = cardsinhand.pop(index)
-            elif c.value + count == 15:
-                suggestion = cardsinhand.pop(index)
-            elif c.value + count == 31:
-                suggestion = cardsinhand.pop(index)
+        for index, c in enumerate(cardsInHand):
+            if testRuns(c):
+                suggestion = cardsInHand.pop(index)
+                break
+            elif testPairs(c):
+                suggestion = cardsInHand.pop(index)
+                break
+            elif c.value + playCount == 15:
+                suggestion = cardsInHand.pop(index)
+            elif c.value + playCount == 31:
+                suggestion = cardsInHand.pop(index)
     return suggestion
 
 
-def p1play(playCount, thePlay):
-    playable = playableCards(p1playhand, playCount)
-    suggestion = suggestedCard(p1playhand, playCount)
+def testPairs(testCard):
+    lastPlayed = thePlay[len(thePlay)-1]
+    if lastPlayed.numb == testCard.numb:
+        return True
+    else:
+        return False
+
+
+def testRuns(testCard):
+
+    hasRun = False
+    index = len(thePlay)
+    if index > 1 and not hasRun:
+        for i in range(3, index+1):
+            evalCards = thePlay[index - i:index].copy()
+            evalCards.append(testCard)
+            cardNumbList = []
+            for l in evalCards:
+                cardNumbList.append(l.numb)
+            sortedCards = sorted(cardNumbList)
+            rangeList = list(range(min(sortedCards),max(sortedCards)+1))
+
+            if sortedCards == rangeList:
+                print("Has Run")
+                hasRun = True
+            else:
+                pass
+    else:
+        pass
+
+    return hasRun
+
+# #
+# # Player action functions
+# #
+
+def p1play():
+
+    playable = playableCards(p1playhand)
+    suggestion = suggestedCard(p1playhand)
     if suggestion is not None:
         playCard = suggestion
     else:
         randCard = playable[randint(0, len(playable) - 1)]
         playCard = p1playhand.pop(randCard)
 
-    playCount += playCard.value
+    addCount = playCard.value
     thePlay.append(playCard)
-    print("P1:",playCard.textPlay(),"Count:",playCount)
 
-    return playCount
+    spc = 14 - len(playCard.textPlay())
+    print("P1:", playCard.textPlay(), " " * spc, "     Count:", addCount + playCount)
 
+    return addCount
 
-def p2play(playCount, thePlay):
-    playable = playableCards(p2playhand, playCount)
-    suggestion = suggestedCard(p2playhand, playCount)
+def p2play():
+
+    playable = playableCards(p2playhand)
+    suggestion = suggestedCard(p2playhand)
     if suggestion is not None:
         playCard = suggestion
     else:
         randCard = playable[randint(0, len(playable) - 1)]
         playCard = p2playhand.pop(randCard)
 
-    playCount += playCard.value
+    addCount = playCard.value
     thePlay.append(playCard)
-    print("P2:", playCard.textPlay(), "Count:", playCount)
 
-    return playCount
+    spc = 14 - len(playCard.textPlay())
+    print("P2:", playCard.textPlay(), " " * spc, "     Count:", addCount + playCount)
+
+    return addCount
 
 
-def checkPlayForPoints(playCount):
+def checkPlayForPoints():
     count = playCount
     play = thePlay
     score = 0
@@ -222,15 +268,10 @@ def checkPlayForPoints(playCount):
         score += 1
         print("31 for two")
     score += playPairs()
+    score += playRuns()
 
     return score
 
-def testPairs(input):
-    lastPlayed = thePlay[len(thePlay)-1]
-    if lastPlayed.numb == input.numb:
-        return True
-    else:
-        return False
 
 def playPairs():
     pairsList = thePlay.copy()
@@ -263,22 +304,53 @@ def playPairs():
     return score
 
 
+def playRuns():
+
+    score = 0
+    scoreList = [0]
+    index = len(thePlay)
+    if index > 2:
+        for i in range(3, index+1):
+            evalCards = thePlay[index - i:index].copy()
+            cardNumbList = []
+            for l in evalCards:
+                cardNumbList.append(l.numb)
+            sortedCards = sorted(cardNumbList)
+            rangeList = list(range(min(sortedCards),max(sortedCards)+1))
+
+            if sortedCards == rangeList:
+                print("Run of", i)
+                scoreList.append(i)
+            else:
+                pass
+
+    else:
+        score = 0
+    score = max(scoreList)
+    return score
+
+# # Test functions
+AVG_TEST = []
+START = time.time()
+
+# #
 # # Game starts
-print("───Game begins".upper())
-createDeck()
-playDeck = list(fullDeck)
+# #
 
-dealer = cutToStart()
 
-print()
+for i in range(0, 100):
 
-# # Prep the play phase
-print("───Play Phase")
+    print("─────────────── GAME BEGINS ───────────────".upper())
+    createDeck()
+    playDeck = list(fullDeck)
 
-for i in range(0, 2):
+    dealer = cutToStart()
+
     roundPhaseOver = False
     while not roundPhaseOver:
-        print( "────",roundCounter, "Begins ─────")
+        print()
+        print()
+        print("───────────", roundCounter, "BEGINS ────────────")
 
         player1cards = []
         player2cards = []
@@ -287,12 +359,10 @@ for i in range(0, 2):
         fullDeck = []
         playDeck = []
 
-
         createDeck()
         playDeck = list(fullDeck)
 
-        for c in playDeck:
-            print(c.textPlay())
+        # # Play begins
         deal()
 
         p2discard()
@@ -301,24 +371,15 @@ for i in range(0, 2):
         p1playhand = player1cards.copy()
         p2playhand = player2cards.copy()
 
-        print("Player 1")
-        for cards in player1cards: print(cards.textPlay())
-        print()
-        print("Player 2")
-        for cards in player2cards: print(cards.textPlay())
-        print()
-        print("Crib")
-        for cards in crib: print(cards.textPlay())
-
         if dealer:
             p1turn = False
         else:
             p1turn = True
 
         if p1turn:
-            print("Player 1 starts")
+            print("                   PLAYER 1 STARTS")
         else:
-            print("Player 2 starts")
+            print("                   PLAYER 2 STARTS")
 
         outOfCards = False
         while not outOfCards:
@@ -342,26 +403,26 @@ for i in range(0, 2):
                         print()
                         thePlay = []
 
-                    elif p1turn and canGo(p1playhand, playCount):
-                        playCount = p1play(playCount, thePlay)
+                    elif p1turn and canGo(p1playhand):
+                        playCount += p1play()
                         if len(thePlay) > 1:
-                            p1score += checkPlayForPoints(playCount)
+                            p1score += checkPlayForPoints()
                         p1turn = False
                     elif p1turn:
                         p1go = True
-
                         p1turn = False
-                    elif not p1turn and canGo(p2playhand, playCount):
-                        playCount = p2play(playCount, thePlay)
+                    elif not p1turn and canGo(p2playhand):
+                        playCount += p2play()
                         if len(thePlay) > 1:
-                            p2score += checkPlayForPoints(playCount)
+                            p2score += checkPlayForPoints()
                         p1turn = True
                     elif not p1turn:
                         p1turn = True
                         p2go = True
 
-        print()
-        print("───Play phase over")
+        print("Player 1 Score -", p1score)
+        print("Player 2 score -", p2score)
+        print("────────────────────── HANDS ─────")
 
         print("Player 1")
         for cards in player1cards: print(cards.textPlay())
@@ -372,11 +433,6 @@ for i in range(0, 2):
         print("Crib")
         for cards in crib: print(cards.textPlay())
 
-        print()
-        print("Player 1 Score -", p1score)
-        print("Player 2 score -", p2score)
-        print("─── Round",roundCounter," Over ───")
-
         if dealer:
             dealer = False
         else:
@@ -384,8 +440,12 @@ for i in range(0, 2):
 
         if p1score >= WIN or p2score >= WIN:
             roundPhaseOver = True
+
+            # # Test function
             with open("Roundtests.txt", "a") as fp:
                 fp.write(str(roundCounter) + ", ")
+            AVG_TEST.append(roundCounter)
+
             roundCounter = 0
             p1score = 0
             p2score = 0
@@ -394,7 +454,12 @@ for i in range(0, 2):
             roundCounter += 1
 
 
+# # Test Functions
+END = time.time()
+DURATION = END - START
 
+print("Average Rounds", mean(AVG_TEST))
+print("Duration", DURATION)
 
 
 
